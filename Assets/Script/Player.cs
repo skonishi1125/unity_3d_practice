12d93e071c97f2c405c6b73e27a2bb3e45b2bcc2
SKONI_NOTE\watas <skonishi1125@gmail.com>
2026-01-10 16:45:47 +0800

    Unityプロジェクトの差し替え。

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Movement")]
+    public PlayerInputSet input;
+    private Vector2 moveInput;
+
+    private void Awake()
+    {
+        input = new PlayerInputSet();
+    }
+
+    private void Start()
+    {
+        input.Player.Movement.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        input.Player.Movement.canceled += ctx => moveInput = Vector2.zero;
+    }
+
+    private void Update()
+    {
+        // プレイヤーの移動
+        transform.position = new Vector3(moveInput.x, transform.position.y, moveInput.y);
+    }
+
+
+
+    // オブジェクトが有効なときだけ入力をうけつけるようにする
+    private void OnEnable()
+    {
+        input.Enable();
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+    }
+
+}
