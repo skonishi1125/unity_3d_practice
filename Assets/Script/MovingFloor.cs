f73049234d5d6c2a5042c50895dd2cae23a17225
SKONI_NOTE\watas <skonishi1125@gmail.com>
2026-01-12 23:35:05 +0800

    wip: 移動床の実装

---
+++ Assets/Script/MovingFloor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class MovingFloor : MonoBehaviour
+{
+    [SerializeField] private float sinValue;
+    [Range(0, 1)]
+    [SerializeField] private float sinMultiplier = .5f;
+    [SerializeField] private float moveSpeed = 2f;
+
+    private void Update()
+    {
+        Vector3 moveVector = transform.position;
+
+        float sinValue = Mathf.Sin(Time.time) * sinMultiplier;
+        Debug.Log(sinValue);
+
+        if (sinValue > 0)
+            moveVector.z = moveVector.z - moveSpeed * Time.deltaTime;
+        else
+            moveVector.z = moveVector.z + moveSpeed * Time.deltaTime;
+
+        transform.position = moveVector;
+
+    }
+
+}
