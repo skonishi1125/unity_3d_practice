7d71fcddde7c6383a12c77c749f32ec7dc310a1a
skonishi <skonishi1125@gmail.com>
2026-01-16 21:31:11 +0900

    wip: HeadAim調整

---
+++ Assets/Script/HeadAimController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+public class HeadAimController : MonoBehaviour
+{
+    private Player player;
+    [SerializeField] private Rig rig;
+    [SerializeField] private MultiAimConstraint headAim;
+    [SerializeField] private GameObject target;
+
+    private void Awake()
+    {
+        player = GetComponentInParent<Player>();
+
+        if (rig == null)
+            rig = GetComponentInChildren<Rig>();
+
+        if (headAim == null)
+            headAim = GetComponentInChildren<MultiAimConstraint>();
+
+        if (target == null)
+            Debug.LogWarning("Target is not assigned.");
+    }
+
+    private void Start()
+    {
+        headAim.weight = 0f;
+    }
+
+    private void Update()
+    {
+
+    }
+
+
+}
